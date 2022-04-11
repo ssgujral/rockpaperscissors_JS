6a8780803e5b50b1374ea13bcf55fb37a99c1878
samgujral <71089547+samgujral@users.noreply.github.com>
2022-04-11 22:45:47 +0100

    Rock Paper Scissors working

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -22,11 +22,63 @@ else if (computer_selection_number == 2)
   } 
 
 
+  
+
+if (user_selection_string == "Rock") 
+{
+  if (computer_selection_string == "Rock")
+  {
+    console.log("The computer also chose Rock. It's a draw!");
+  }
+  else if (computer_selection_string == "Paper") 
+  {
+    console.log("The computer chose Paper. You lose!");
+  }
+  else if (computer_selection_string == "Scissors")
+  {
+    console.log ("The computer chose Scissors. You win!!!");
+  }
+}
+
+else if (user_selection_string == "Paper") 
+{
+  if (computer_selection_string == "Rock")
+  {
+    console.log("The computer chose Rock. You win!!!");
+  }
+  else if (computer_selection_string == "Paper")
+  {
+    console.log("The computer also chose Paper. It's a draw!");
+  }
+  else if (computer_selection_string == "Scissors")
+  {
+    console.log("The computer chose Scissors. You lose!");
+  }
+}
+
+else if (user_selection_string == "Scissors")
+{
+  
+  if (computer_selection_string == "Rock")
+  {
+    console.log("The computer chose Rock. You lose!");
+  }
+  
+  else if (computer_selection_string == "Paper")
+  {
+    console.log("The computer chose Paper. You win!!!");
+  }
+
+  else if (computer_selection_string == "Scissors")
+  {
+    console.log("The computer also chose Scissors. It's a draw!");
+  }
+}
+
+
 
   console.log(" ");
   console.log (computer_selection_number);
   console.log (computer_selection_string);
   console.log (user_selection_string);
   console.log (" ");
-
-
